c02048b9962a8ad14b4de5f0986faf4732ece93c
jose <maldonadojose201422@gmail.com>
2022-04-19 23:56:43 -0300

    comienzo test

---
+++ src/tests/demo.test.js
@@ -0,0 +1,13 @@
+describe('Pruebas en el archivo demo.test.js',()=>{
+    test('Deben ser iguales los string', () => { 
+        //1.incializacion
+        const mensaje = "Hola Mundo";
+        //2.estimulo
+        const mensaje2 = "Hola Mundo!s";
+    
+        //3.Observar comportamiento
+        expect(mensaje).toBe(mensaje2);
+        
+    })
+});
+
